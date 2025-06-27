e79acc501ada30e155c06adbbd965b60e7228c6f
abncalut <abnaclut@gmail.com>
2025-06-27 23:01:41 +0100

    fix, added extra text

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 int main()
 {
-	std::cout << "hello world!"; << std::endl;
+	std::cout << "hello world!" << std::endl;
+	std::cout << "fixed" << std::endl;
+	std::cout << "text" << std::endl;
 	return 0;
 }
