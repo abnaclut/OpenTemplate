5ff878889e8b49d29a1036d195f036d45819f923
abncalut <abnaclut@gmail.com>
2025-07-01 18:38:24 +0100

    restructured directories

---
+++ cmake-build-debug/CMakeFiles/openTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openTemplate.dir/link.d"
+  "CMakeFiles/openTemplate.dir/src/Renderer/ShaderProgram.cpp.o"
+  "CMakeFiles/openTemplate.dir/src/Renderer/ShaderProgram.cpp.o.d"
+  "CMakeFiles/openTemplate.dir/src/Resources/ResourceManager.cpp.o"
+  "CMakeFiles/openTemplate.dir/src/Resources/ResourceManager.cpp.o.d"
+  "CMakeFiles/openTemplate.dir/src/main.cpp.o"
+  "CMakeFiles/openTemplate.dir/src/main.cpp.o.d"
+  "bin/openTemplate"
+  "bin/openTemplate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/openTemplate.dir/src/Resources/ResourceManager.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/openTemplate.dir/src/Resources/ResourceManager.cpp.o: \
+ /home/user/openTemplate/src/Resources/ResourceManager.cpp \
+ /usr/include/stdc-predef.h
