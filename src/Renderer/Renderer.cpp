b969786bcd12c0120d970e0e67940668b7257b13
abncalut <abnaclut@gmail.com>
2025-09-23 17:37:02 +0100

    prototype finished but not ready for release

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -1,13 +1,13 @@
 #include "Renderer.h"
 
-namespace RenderEngine {
-
+namespace RenderEngine
+{
   void Renderer::draw(const VertexArray& vertexArray, const IndexBuffer& indexBuffer, const ShaderProgram& shader)
   {
     shader.use();
     vertexArray.bind();
     indexBuffer.bind();
-
+    //FIXME! Pass hardcoded params as a specifiable vars!!!
     glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_INT, nullptr);
   }
 
@@ -16,16 +16,18 @@ namespace RenderEngine {
   void Renderer::setDepthTest(const bool enable)
   {
     if (enable) { glEnable(GL_DEPTH_TEST); }
-    else { glDisable(GL_DEPTH_TEST); }
+    else        { glDisable(GL_DEPTH_TEST);}
   }
 
-  void Renderer::clear() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
+  void Renderer::clear() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); } //FIXME pass params by variable?
 
   void Renderer::setViewport(const unsigned int width,
                              const unsigned int height,
                              const unsigned int leftOffset,
                              const unsigned int bottomOffset)
-  { glViewport(leftOffset, bottomOffset, width, height); }
+  {
+    glViewport(leftOffset, bottomOffset, width, height);
+  }
 
   std::string Renderer::getRendererStr() { return reinterpret_cast<const char*>(glGetString(GL_RENDERER)); }
   std::string Renderer::getVersionStr()  { return reinterpret_cast<const char*>(glGetString(GL_VERSION));  }
