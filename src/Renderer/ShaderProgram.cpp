2bf8b7af8a49b86d3ad4ff0549b02d4f5b0867e0
abncalut <abnaclut@gmail.com>
2025-09-23 18:13:15 +0100

    finished prototype but not ready for release

--- src/Renderer/ShaderProgram.cpp
+++ src/Renderer/ShaderProgram.cpp
@@ -1,30 +1,29 @@
 #include "ShaderProgram.h"
-#include <iostream>
-#include <ostream>
-#include <glm/gtc/type_ptr.hpp>
 
 namespace RenderEngine
 {
-    //CONSTRUCTORS
     ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
     {
         //CREATE VERTEX SHADER
         GLuint vertexShaderID;
+        //FIXME add a typedef for GlVertexShader_type = GL_VERTEX_SHADER
         if (createShader(vertexShader, GL_VERTEX_SHADER, vertexShaderID) == false)
         {
-            std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n";
+            std::cerr << "Vertex Shader compilation FAILED!\n";
             return;
         }
 
         //CREATE FRAGMENT SHADER
         GLuint fragmentShaderID;
+        //FIXME add a typedef for GlFragmentShader_type = GL_FRAGMENT_SHADER
         if (createShader(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderID) == false)
         {
-            std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n";
+            std::cerr << "Fragment Shader compilation FAILED!\n";
             glDeleteShader(vertexShaderID);
             return;
         }
 
+        //TODO move to a separate function! void linkShaders()
         //LINK SHADERS
         m_ID = glCreateProgram();
         glAttachShader(m_ID, vertexShaderID);
@@ -38,19 +37,16 @@ namespace RenderEngine
         {
             GLchar infoLog[1024];
             glGetProgramInfoLog(m_ID, 1024, nullptr, infoLog);
-            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            std::cerr << "Shader Program LINKING FAILED! Info: \n" << infoLog << "\n";
             glDeleteProgram(m_ID);
             glDeleteShader(vertexShaderID);
-        }
-        else
-        {
-            m_isCompiled = true;
-        }
+        } else { m_isCompiled = true; }
 
         glDeleteShader(vertexShaderID);
         glDeleteShader(fragmentShaderID);
     }
-    //OPERATORS
+    ShaderProgram::~ShaderProgram() { glDeleteProgram(m_ID); }
+
     ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram) noexcept
     {
         glDeleteProgram(m_ID);
@@ -71,9 +67,9 @@ namespace RenderEngine
         shaderProgram.m_isCompiled = false;
     }
 
-    //SHADER CREATION IMPLEMENTATION
-    bool ShaderProgram::createShader(const std::string& source, GLenum shaderType, GLuint& shaderID)
+    bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID)
     {
+        //FIXME!!! Has to be remade properly. Remove hardcoding, add error-checks, ....
         shaderID = glCreateShader(shaderType);
         const char* code = source.c_str();
         glShaderSource(shaderID, 1, &code, nullptr);
@@ -87,33 +83,25 @@ namespace RenderEngine
         {
             GLchar infoLog[1024];
             glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
-            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+            std::cout << "Error! Shader compilation failed! Info: \n" << infoLog << std::endl;
             return false;
         }
         return true;
     }
 
-    //DESTRUCTOR
-    ShaderProgram::~ShaderProgram()
-    {
-        glDeleteProgram(m_ID);
-    }
-
     void ShaderProgram::use() const { glUseProgram(m_ID); }
-    void ShaderProgram::unuse() { glUseProgram(0); }
-
+    void ShaderProgram::unuse()     { glUseProgram(0); }
     void ShaderProgram::setInt(const std::string& name, const GLint value) const
     {
         glUniform1i(glGetUniformLocation(m_ID, name.c_str()), value);
     }
-
     void ShaderProgram::setFloat(const std::string& name, const GLfloat value) const
     {
         glUniform1f(glGetUniformLocation(m_ID, name.c_str()), value);
     }
-
     void ShaderProgram::setMatrix4(const std::string& name, const glm::mat4& matrix) const
     {
+        //FIXME add a bool transpose param
         glUniformMatrix4fv(glGetUniformLocation(m_ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
     }
 }
--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -1,28 +1,24 @@
 #pragma once
-#ifndef SHADER_PROGRAM_H
-#define SHADER_PROGRAM_H
 #include <string>
 #include <glm/glm.hpp>
 #include <glad/glad.h>
-#endif //SHADER_PROGRAM_H
+// ReSharper disable once CppUnusedIncludeDirective USED IN .CPP
+#include <iostream>
+#include <ostream>
+// ReSharper disable once CppUnusedIncludeDirective USED IN .CPP
+#include <glm/gtc/type_ptr.hpp>
 
 namespace RenderEngine
 {
     class ShaderProgram
     {
     public:
-        //CONSTRUCTORS
         ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
-        //NOT ALLOWING EMPTY SHADER
         ShaderProgram() = delete;
-        //NOT ALLOWING SHADER-COPY / SAME IDs
         ShaderProgram(ShaderProgram&) = delete;
-        //NOT ALLOWING SHADER ASSIGMENT / SAME IDs
         ShaderProgram& operator=(const ShaderProgram&) = delete;
-        //moving operators
         ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
         ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
-        //DESTRUCTORS
         ~ShaderProgram();
         void setInt(const std::string& name, GLint value) const;
         void setFloat(const std::string& name, GLfloat value) const;
